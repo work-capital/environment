79b8e364a897f652699da1d5852f2eb032cde55c
Henry Hazan <henry@work.capital>
2016-07-07 14:48:40 +0300

    compose

---
+++ docker/install-docker-compose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# https://docs.docker.com/compose/install/
+
+
+
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
