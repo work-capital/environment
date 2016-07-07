104ab83fb3b167e03dfd1b0170f59c90b4659579
Henry Hazan <henry@work.capital>
2016-07-07 14:47:53 +0300

    docker compose

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # https://docs.docker.com/engine/installation/linux/debian/#/debian-jessie-80-64-bit
 apt-get update
-apt-get install apt-transport-https ca-certificates
+apt-get install apt-transport-https ca-certificates -y
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-cache policy docker-engine
 apt-get update
-apt-get install docker-engine
+apt-get install docker-engine -y
 
