6c9b212481699847dcccb925993ab1c604046b8b
Henry Hazan <henry@work.capital>
2017-04-23 11:06:53 +0300

    divide ubuntu & debian configs

---
+++ keyboard/map-keyboard-debian.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# x.613.ein..053
+setxkbmap -option \
+	-option caps:swapescape \
+  -option altwin:ctrl_win
+	# -option ctrl:swap_lwin_lctl \
+	# -option ctrl:swap_rwin_rctl 
+	#-option altwin:swap_alt_win
+
