633791d884b4c07cff9f9cf115caed488f45d91f
Henry Hazan <henry@work.capital>
2017-04-25 15:43:11 +0300

    activate tap click

--- keyboard/map-keyboard-ubuntu.sh
+++ keyboard/map-keyboard-ubuntu.sh
@@ -6,3 +6,6 @@ setxkbmap -option \
 	-option ctrl:swap_rwin_rctl 
 	#-option altwin:swap_alt_win
 
+# configure tap to click in trackpad
+# as described here: https://askubuntu.com/questions/403113/how-do-you-enable-tap-to-click-via-command-line-with-xmodmap
+xinput set-prop "bcm5974" "libinput Tapping Enabled" 1
