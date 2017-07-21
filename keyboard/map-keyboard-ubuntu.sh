d6870d700f6e7279fe476ba6ee95baab21fe6436
Henry Hazan <henry@work.capital>
2017-07-21 00:57:09 -0400

    add tap

--- keyboard/map-keyboard-ubuntu.sh
+++ keyboard/map-keyboard-ubuntu.sh
@@ -6,7 +6,3 @@ setxkbmap -option \
 	-option ctrl:swap_rwin_rctl 
 	#-option altwin:swap_alt_win
 
-# configure tap to click in trackpad
-# as described here: https://askubuntu.com/questions/403113/how-do-you-enable-tap-to-click-via-command-line-with-xmodmap
-# remember to apt-get install xinput
-xinput set-prop "bcm5974" "libinput Tapping Enabled" 1
