517f4d61b28999c482daadf2e4ed316120966a92
Shulamit Charedim <shulamit@work.capital>
2017-09-04 09:10:52 +0300

    fix keyboard

--- keyboard/map-keyboard-debian.sh
+++ keyboard/map-keyboard-debian.sh
@@ -2,7 +2,5 @@
 # x.613.ein..053
 setxkbmap -option \
 	-option caps:swapescape \
-	-option ctrl:swap_lwin_lctl \
-	-option ctrl:swap_rwin_rctl \
-	-option altwin:swap_alt_win
+  -option altwin:ctrl_alt_win
 
--- video/two-monitors-ajust.sh
+++ video/two-monitors-ajust.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # swap right with left monitor
-xrandr --output DVI-I-1 --right-of VGA-1
+# xrandr --output DVI-I-1 --right-of VGA-1
